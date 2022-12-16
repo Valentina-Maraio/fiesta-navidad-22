f65fd84aadf5fc256de98a91f68fb665b1f3a72d
Valentina-Maraio <valentina.maraio89@gmail.com>
2022-12-16 03:11:52 +0100

    pero porfavooor

--- src/component/title/Title.js
+++ src/component/title/Title.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import '../../App.css'
+import React from "react";
+import "../../App.css";
 
 const Title = () => {
   return (
     <>
-    <div className='App_title'>
-        <h1><i>La Cocina</i></h1>
-    </div>
+      <div className="App_title">
+          <h1>
+            <i>La Cocina</i>
+          </h1>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
